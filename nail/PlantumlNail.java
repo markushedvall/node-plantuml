c1684169068ba630d07e7b5dca6b12f2ab9fb26e
Markus Hedvall <mackanhedvall@gmail.com>
2015-12-06 14:40:50 +0100

    Remove lock when decoding while using nailgun
    Since plantuml run in a single process when using nailgun, it's
    protected by a lock. The lock was also set when decoding, making
    it impossible to pipe the stream to another plantuml operation in
    nailgun.

--- nail/PlantumlNail.java
+++ nail/PlantumlNail.java
@@ -9,11 +9,25 @@ import net.sourceforge.plantuml.Run;
 
 public class PlantumlNail {
   public static void nailMain(NGContext context) throws IOException, InterruptedException {
-    synchronized (PlantumlNail.class) {
-      File cwd = new File(context.getWorkingDirectory());
-      FileSystem.getInstance().setCurrentDir(cwd);
-      Run.main(context.getArgs());
-      context.exit(0);
+    boolean decode = false;
+    for (String arg : context.getArgs()) {
+      if (arg.equals("-decodeurl")) {
+        decode = true;
+        break;
+      }
     }
+
+    if (!decode) {
+      synchronized (PlantumlNail.class) {
+        File cwd = new File(context.getWorkingDirectory());
+        FileSystem.getInstance().setCurrentDir(cwd);
+        Run.main(context.getArgs());
+      }
+    } else {
+      // Don't synchronize when decoding
+      Run.main(context.getArgs());
+    }
+
+    context.exit(0);
   }
 }
