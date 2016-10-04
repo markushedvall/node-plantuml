8b3765d56d14344950ba9edbb65cda032a053d16
Markus Hedvall <mackanhedvall@gmail.com>
2016-10-04 22:33:38 +0200

    Make prepareation of fixtures use PLANTUML_HOME

--- test/fixtures/prepare.js
+++ test/fixtures/prepare.js
@@ -5,7 +5,8 @@ var path = require('path')
 var shell = require('shelljs')
 
 var JAVA = 'java -Djava.awt.headless=true -jar'
-var PLANTUML_JAR = path.join(__dirname, '../../vendor/plantuml.jar')
+var INCLUDED_PLANTUML_JAR = path.join(__dirname, '../../vendor/plantuml.jar')
+var PLANTUML_JAR = process.env.PLANTUML_HOME || INCLUDED_PLANTUML_JAR
 var TEST_PUML = path.join(__dirname, 'test.puml')
 
 shell.exec(JAVA + ' ' + PLANTUML_JAR + ' ' + TEST_PUML)
