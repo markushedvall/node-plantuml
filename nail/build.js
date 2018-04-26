4cabd85a45e17fe3d075ea4c8295966277e837c5
Markus Hedvall <mackanhedvall@gmail.com>
2018-04-26 20:11:27 +0800

    Update standard to 11.x

--- nail/build.js
+++ nail/build.js
@@ -4,12 +4,13 @@
 var path = require('path')
 var shell = require('shelljs')
 
-var PLANTUML_JAR = path.join(__dirname, '../vendor/plantuml.jar')
-var NAILGUN_JAR = path.join(__dirname, '../node_modules/node-nailgun-server/vendor/nailgun.jar')
-var PLANTUML_NAIL_JAR = path.join(__dirname, 'plantumlnail.jar')
-var PLANTUML_NAIL_JAVA = path.join(__dirname, 'PlantumlNail.java')
+var DIR = __dirname
+var PLANTUML_JAR = path.join(DIR, '../vendor/plantuml.jar')
+var NAILGUN_JAR = path.join(DIR, '../node_modules/node-nailgun-server/vendor/nailgun.jar')
+var PLANTUML_NAIL_JAR = path.join(DIR, 'plantumlnail.jar')
+var PLANTUML_NAIL_JAVA = path.join(DIR, 'PlantumlNail.java')
 var PLANTUML_NAIL_CLASS = 'PlantumlNail.class'
 
 shell.exec('javac -cp ' + PLANTUML_JAR + ':' + NAILGUN_JAR + ' ' + PLANTUML_NAIL_JAVA)
-shell.exec('jar -cfM ' + PLANTUML_NAIL_JAR + ' -C ' + __dirname + ' ' + PLANTUML_NAIL_CLASS)
-shell.exec('rm ' + path.join(__dirname, PLANTUML_NAIL_CLASS))
+shell.exec('jar -cfM ' + PLANTUML_NAIL_JAR + ' -C ' + DIR + ' ' + PLANTUML_NAIL_CLASS)
+shell.exec('rm ' + path.join(DIR, PLANTUML_NAIL_CLASS))
