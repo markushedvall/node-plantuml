ded712322c42d2c213fe6322e7f0e9e6c5b6168e
Markus Hedvall <mackanhedvall@gmail.com>
2019-01-17 20:44:26 +0100

    Update devDependencies to newer versions

--- test/node-plantuml-cmd-test.js
+++ test/node-plantuml-cmd-test.js
@@ -11,8 +11,8 @@ var PUML = 'node ' + INDEX_JS
 describe('node-plantuml CLI', function () {
   describe('#encode', function () {
     it('should encode "A -> B: Hello"', function () {
-      var cli = shell.exec(PUML + ' encode "A -> B: Hello"', {silent: true})
-      expect(cli.output).to.equal('SrJGjLDmibBmICt9oGS0\n')
+      var cli = shell.exec(PUML + ' encode "A -> B: Hello"', { silent: true })
+      expect(cli.stdout).to.equal('SrJGjLDmibBmICt9oGS0\n')
     })
   })
 })
